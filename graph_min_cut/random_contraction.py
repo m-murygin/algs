06a4d2462bb22ce87b2413ea247ae4009764fe6f
Maksim Murygin <muriginm@gmail.com>
2017-03-25 23:34:23 +0700

    implement base for random contraction

--- graph_min_cut/random_contraction.py
+++ graph_min_cut/random_contraction.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
 import argparse
-import pprint
 from random import randint
-
-pp = pprint.PrettyPrinter(indent=2)
 
 parser = argparse.ArgumentParser(
     description='Search min cut in the input graph')
@@ -18,11 +15,11 @@ class Vertex(object):
     def __init__(self, _id):
         super(Vertex, self).__init__()
         self.id = _id
-        self.edges = []
+        self.edges = list()
 
     def __str__(self):
         edges_str = ', '.join([str(e) for e in self.edges])
-        return '%s [%s]' % (self.id, edges_str)
+        return '(%s) [%s]' % (self.id, edges_str)
 
 class Edge(object):
     def __init__(self, head, tail):
@@ -31,25 +28,68 @@ class Edge(object):
         self.tail = tail
 
     def __str__(self):
-        return '%s:%s' % (self.head, self.tail)
+        return '%s:%s' % (self.head.id, self.tail.id)
+
+
+def find_min_cut(vertices, edges):
+    """Fins min cut
+
+    Args:
+        vertices (dict): list of graph vertices
+        edges (dict): list of graph edges
+
+    Returns:
+        number: min cut edges count
+    """
+    remains_iterations = len(vertices) - 2
+    while remains_iterations > 0:
+        contracted_edge_index = randint(0, len(edges)-1)
+        contracted_edge = edges[contracted_edge_index]
+
+        base_vertex = contracted_edge.head
+        contracted_vertex = contracted_edge.tail
+
+        for edge in contracted_vertex.edges:
+            # remove self-referenced edges
+            if edge.head.id == base_vertex.id or edge.tail.id == base_vertex.id:
+                base_vertex.edges.remove(edge)
+                edges.remove(edge)
+            elif edge.head.id == contracted_vertex.id:
+                edge.head = base_vertex
+            else:
+                edge.tail = base_vertex
+
+        vertices.remove(contracted_vertex)
+        edges.remove(contracted_edge)
+
+        remains_iterations -= 1
+
+    return len(vertices[0].edges)
 
 
 def parse_input_file(filename):
-    vertices = []
-    edges = []
+    vertices = list()
+    edges = list()
 
     with open(filename) as f:
-        for line in f:
-            line_list = [int(x) for x in line.split(' ')]
-            vertex_id = line_list.pop(0)
-            vertex = Vertex(vertex_id)
+        lines = f.readlines()
 
-            for edge_tail in line_list:
-                edge = Edge(vertex_id, edge_tail)
+        # fill vertices
+        for line in lines:
+            vertex_id = int(line.split(' ')[0])
+            vertices.append(Vertex(vertex_id))
+
+        # fill edges
+        for line in lines:
+            line_ints = [int(x) for x in line.split(' ')]
+            # get edge vertex
+            head_vertex = vertices[line_ints.pop(0) - 1]
+
+            for tail_id in line_ints:
+                tail_vertex = vertices[tail_id - 1]
+                edge = Edge(head_vertex, tail_vertex)
                 edges.append(edge)
-                vertex.edges.append(edge)
-
-            vertices.append(vertex)
+                head_vertex.edges.append(edge)
 
     return (vertices, edges)
 
@@ -58,10 +98,12 @@ def main():
     vertices = parse_result[0]
     edges = parse_result[1]
 
-    for v in vertices:
-        print v
-    for e in edges:
-        print e
+    # for v in vertices:
+    #     print v
+    # for e in edges:
+    #     print e
+
+    print find_min_cut(vertices, edges)
 
 if __name__ == '__main__':
     main()
